0f34ea7e3a328d4bf74fb06c98ee049baedd4e35
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-09-23 16:55:44 -0700

    add actors.js

--- six/src/components/App.js
+++ six/src/components/App.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './App.css';
+import Actors from './actors';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Actors />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ six/src/components/actors.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Actors() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [Actors, setActors] = useState([]);
+}
+
+export default Actors
