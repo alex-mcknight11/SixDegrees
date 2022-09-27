2dc4d1d252f3c88f81f3e1d53c40885abe0cad51
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-09-26 22:09:41 -0700

    organize project

--- six/src/components/actors.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-function Actors() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [Actors, setActors] = useState([]);
-}
-
-export default Actors
---
+++ src/components/actors.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Actors() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [Actors, setActors] = useState([]);
+}
+
+useEffect(() => {
+  fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    .then(response => response.json())
+    .then((jsonifiedResponse) => {
+        setTopStories(jsonifiedResponse.results)
+        setIsLoaded(true)
+      })
+    .catch((error) => {
+      setError(error)
+      setIsLoaded(true)
+    });
+  }, [])
+}
+
+export default TopStories;
+
+export default Actors
