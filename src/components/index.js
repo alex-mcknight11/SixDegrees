0dd2493743689ebad34647d3f9cd84266feac53d
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-02 22:09:31 -0700

    prototype random actor generator

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,13 @@
+import actors from "actor-array";
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function getTwoRandomActorIds() {
-  Math.random()
+  const i = getRandomInt(actors.length);
+  const j = getRandomInt(actors.length);
+    return [actors[i], actors[j]];
 }
+
+//fix so same actors are not in random integer
