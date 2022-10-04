cec0fd1303303687b42a0c4a3a312b8dd04224b9
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-04 16:03:16 -0700

    WIP API research + work on API call for actor credits

---
+++ src/components/get-movie.js
@@ -0,0 +1,6 @@
+export default class TMDBService {
+  static async getMoviesFromActor(movies) {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/person/${}/movie_credits?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    }
+  }
