ba122f64bf07fd4a339ae280d731e37fabb571ad
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-05 15:40:39 -0700

    Research + fix dependancies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from "./components/header";
 import actors from './components/actor-array';
+import MovieDropdown from './components/movie-dropdown';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      
     </React.Fragment>
   );
 }
--- src/components/movie-dropdown.jsx
+++ src/components/movie-dropdown.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import TMDBService from './components/get-movie';
-import { Dropdown } from 'semantic-ui-react'
+  // import React from 'react'
+  // import TMDBService from './components/get-movie';
+  // import { Dropdown } from 'semantic-ui-react'
 
-const movieOptions = [ 
-  {key: TMDBService}
-]
 
-const MovieDropdown = () => (
-  <Dropdown
-    placeholder='Select Movie'
-    fluid
-    search
-    selection
-    options={movieOptions}
-  />
-)
+  // const MovieDropdown = () => (
+  //   <Dropdown
+  //     placeholder='Select Movie'
+  //     fluid
+  //     search
+  //     selection
+  //     options={movieOptions}
+  //   />
+  // )
 
-export default MovieDropdown
+  // export default MovieDropdown
