de33a0870d9a83f979d55bcc089a818c2e1151e8
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-05 15:32:16 -0700

    Research + fix dependancies

--- src/components/movie-dropdown.jsx
+++ src/components/movie-dropdown.jsx
@@ -1,3 +1,19 @@
 import React from 'react'
 import TMDBService from './components/get-movie';
+import { Dropdown } from 'semantic-ui-react'
 
+const movieOptions = [ 
+  {key: TMDBService}
+]
+
+const MovieDropdown = () => (
+  <Dropdown
+    placeholder='Select Movie'
+    fluid
+    search
+    selection
+    options={movieOptions}
+  />
+)
+
+export default MovieDropdown
