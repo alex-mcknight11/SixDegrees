29d68b30cb760db7ec66635cdea54631f6720a0e
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-05 16:44:22 -0700

    Research + fix actor photo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from "./components/header";
 import actors from './components/actor-array';
 import MovieDropdown from './components/movie-dropdown';
+import ActorPhoto from './components/actor-photo';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <Movie-Dropdown />
+      <ActorPhoto />
     </React.Fragment>
   );
 }
--- src/components/actor-photo.js
+++ src/components/actor-photo.js
@@ -4,7 +4,7 @@ export default class ActorPhoto {
   static async GetActorPhoto(actorID) {
     try {
       const response = await axios.get(`https://api.themoviedb.org/3/person/${actorID}/images?api_key=${process.env.REACT_APP_API_KEY}`)
-      return (response.data.cast.map(credit => ({id:credit.id, title:credit.title}) ))
+      return (response.data.cast.map(credit => ({id:credit.id, image:credit.image}) ))
     }catch(e){}
     }
   }
