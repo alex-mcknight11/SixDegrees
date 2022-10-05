f18e9b2f3e577b5f56b3352c17696378cd9b3fd6
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-10-05 11:28:26 -0700

    WIP extension research + bug fixes

---
+++ src/components/movie-dropdown.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import TMDBService from './components/get-movie';
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import actors from './components/actor-array';
 import TMDBService from './components/get-movie';
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
